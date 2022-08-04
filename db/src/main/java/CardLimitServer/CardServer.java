9ec3f88964fa043a29246e497a280221bfba3aaf
emircangun <gundogdu.emircan@outlook.com>
2022-08-04 15:31:17 +0300

    Implemented gRPC server

---
+++ db/src/main/java/CardLimitServer/CardServer.java
@@ -0,0 +1,38 @@
+package cardlimitserver;
+
+import cardlimitservice.CardServiceImpl;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.protobuf.services.ProtoReflectionService;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class CardServer {
+    public Server server;
+
+    public void start() throws IOException {
+        // port number is hard-coded
+        // it will be nice to get the port number from properties file
+        int port = 9091;
+
+        server = ServerBuilder.forPort(port)
+                .addService(new CardServiceImpl())
+                .addService(ProtoReflectionService.newInstance())
+                .build()
+                .start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                System.err.println("Shutting down gRPC server.");
+                try {
+                    server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+                } catch (InterruptedException e) {
+                    e.printStackTrace(System.err);
+                }
+            }
+        });
+    }
+}
