6ab5818b504b45c90000e778f365cd58faa68226
emircangun <gundogdu.emircan@outlook.com>
2022-08-04 15:37:35 +0300

    Implemented main function that starts the server

---
+++ db/src/main/java/CardLimitApplication/CardLimitApplication.java
@@ -0,0 +1,16 @@
+package CardLimitApplication;
+
+import CardLimitServer.CardServer;
+import java.io.IOException;
+
+public class CardLimitApplication {
+
+    // exceptions do not be handled
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final CardServer cardServer = new CardServer();
+        cardServer.start();
+        cardServer.server.awaitTermination();
+    }
+
+
+}
