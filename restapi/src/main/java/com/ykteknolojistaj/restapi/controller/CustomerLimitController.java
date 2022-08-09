a60b73fefe2c8cf2ecfc802f156d01e8afb0a58c
emircangun <gundogdu.emircan@outlook.com>
2022-08-10 00:38:45 +0300

    Handled gRPC runtime error and updated endpoint

--- restapi/src/main/java/com/ykteknolojistaj/restapi/controller/CustomerLimitController.java
+++ restapi/src/main/java/com/ykteknolojistaj/restapi/controller/CustomerLimitController.java
@@ -19,32 +19,26 @@ import java.util.List;
 @Slf4j
 @Validated
 @RequiredArgsConstructor
-
+@RequestMapping(path = "/api")
 public class CustomerLimitController {
     @Autowired
     private final CardClient cardService;
-    @GetMapping(path = "/get-cards", produces = "application/json")
-    public List<CardModel> getCardsApi(@RequestParam(value = "customer_no") String customer_no) {
 
-        List<Card> cardModels = cardService.receiveCards(customer_no);
-        System.out.println(cardModels);
-        CardList cardList = new CardList(cardModels);
+    @GetMapping(path = "/getCards", produces = "application/json")
+    public List<CardModel> getCardsApi(@RequestParam(value = "customer_no") String customer_no) {
+        CardList cardList = new CardList();
+
+        try {
+            List<Card> cardModels = cardService.receiveCards(customer_no);
+            System.out.println(cardModels);
+            cardList.copyProtoCardArray(cardModels);
+        } catch (io.grpc.StatusRuntimeException grpcRuntimeException) {
+            System.out.println("Error while fetching the data from the database microservice.");
+            System.out.println(grpcRuntimeException);
+        }
+
         return cardList.getCards();
     }
 
 
 }
-
-
-/*
-    @PostMapping("/getLimit")
-    public ResponseEntity<CustomerLimitOutput> getCustomerLimitOutput(@RequestBody CustomerInfo customer_no)
-
-    { if (customer_no.getCustomer_no()=="")
-    {
-        return new ResponseEntity("Costumer No alanları dolu olmalıdır", HttpStatus.BAD_REQUEST);
-    }
-        return new ResponseEntity("Costumer Kart ve Limiti:... ", HttpStatus.OK);
-    }
-
-  */
