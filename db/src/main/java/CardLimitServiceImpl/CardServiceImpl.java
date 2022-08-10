0ff9ba15aadc5929037996e8a340b04902a780f3
emircangun <gundogdu.emircan@outlook.com>
2022-08-10 10:59:09 +0300

    Handled NoSuchElementException and fixed the package name

---
+++ db/src/main/java/CardLimitServiceImpl/CardServiceImpl.java
@@ -0,0 +1,54 @@
+package CardLimitServiceImpl;
+
+import com.ykteknolojistaj.protointerface.Card;
+import com.ykteknolojistaj.protointerface.CardRequest;
+import com.ykteknolojistaj.protointerface.CardResponse;
+import com.ykteknolojistaj.protointerface.CardServiceGrpc;
+import dao.CardDao;
+import io.grpc.stub.StreamObserver;
+
+import net.devh.boot.grpc.server.service.GrpcService;
+
+import java.util.NoSuchElementException;
+
+@GrpcService
+public class CardServiceImpl extends CardServiceGrpc.CardServiceImplBase {
+
+    private CardDao cardDao = new CardDao(); // Database connection is done in cardDao
+
+    /**
+     * Overriding Protobuf getCards service. Getting customerNo from the request and
+     * using it by calling cardDao.
+     * @param request Request coming from REST API
+     * @param responseObserver
+     */
+    @Override
+    public void getCards(CardRequest request, StreamObserver<CardResponse> responseObserver) {
+        System.out.println("Request received from client: " + request);
+
+        CardResponse.Builder builder = CardResponse.newBuilder();
+
+        try {
+            //CardEntity.Card selectedCard = cardService.getCardLimitByCustomerNo(new BigDecimal((request.getCustomerNo())));
+            CardEntity.Card selectedCard = cardDao.findByCustomerNo(request.getCustomerNo());
+
+            // Building only one card from the coming card
+            builder.addCards(
+                    Card.newBuilder().
+                            setCardNo(selectedCard.getCardNo().toString()).
+                            setLimit(selectedCard.getLimit().doubleValue()).
+                            build()
+            );
+        } catch (NoSuchElementException noElementException) {
+            System.out.println(noElementException);
+        }
+
+        System.out.println(builder);
+
+        // building the response to REST API
+        CardResponse response = builder.build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+}
