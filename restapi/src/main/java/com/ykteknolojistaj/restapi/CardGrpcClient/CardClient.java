553abf75faa1b2ac44edc9c20eb3e45fc690e34b
emircangun <gundogdu.emircan@outlook.com>
2022-08-11 12:20:16 +0300

    Added some comments and fixed unused import warning

--- restapi/src/main/java/com/ykteknolojistaj/restapi/CardGrpcClient/CardClient.java
+++ restapi/src/main/java/com/ykteknolojistaj/restapi/CardGrpcClient/CardClient.java
@@ -1,11 +1,9 @@
 package com.ykteknolojistaj.restapi.CardGrpcClient;
 
-
 import com.ykteknolojistaj.protointerface.Card;
 import com.ykteknolojistaj.protointerface.CardRequest;
 import com.ykteknolojistaj.protointerface.CardResponse;
 import com.ykteknolojistaj.protointerface.CardServiceGrpc;
-import com.ykteknolojistaj.restapi.controller.CustomerLimitController;
 import com.ykteknolojistaj.restapi.model.LoggingMessage;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.apache.logging.log4j.Level;
@@ -23,25 +21,32 @@ public class CardClient {
 
     private static final Logger LOG = LogManager.getLogger(CardClient.class.getName());
 
+    /**
+     * Getting all cards of the given customer no by requesting it from the database microservice
+     * @param customer_no given customer no to request from the database microservice
+     * @param corrID unique id for the request
+     * @return list of cards of the given customer no
+     */
     public List<Card> receiveCards(String customer_no, String corrID)
     {
-
+        // logging before building the request
         LoggingMessage loggingMessage = new LoggingMessage(customer_no, corrID, "CardClient got a request, now sending request to db", "CardClient", "start");
         String logMessage = loggingMessage.toString();
         LOG.log(Level.INFO, logMessage);
 
-
+        // building the request from the unique id and the customer no
         CardRequest request = CardRequest.newBuilder()
                 .setCustomerNo(customer_no)
                 .setCorrID(corrID)
                 .build();
 
+        // getting response from the service
         CardResponse response = cardServiceStub.getCards(request);
 
+        // logging after the response
         LoggingMessage loggingMessage2 = new LoggingMessage(customer_no, corrID, "CardClient got a response from db and the returned card(s) are: " + response.getCardsList().toString(), "CardClient", "end");
         logMessage = loggingMessage2.toString();
         LOG.log(Level.INFO, logMessage);
-
 
         return response.getCardsList();
     }
