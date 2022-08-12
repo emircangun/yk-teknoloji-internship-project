c6a5398ebacd9f1ec403027a4435946a396ec865
emircangun <gundogdu.emircan@outlook.com>
2022-08-12 20:54:00 +0300

    Implemented exception handling for API

--- restapi/src/main/java/com/ykteknolojistaj/restapi/controller/CustomerLimitController.java
+++ restapi/src/main/java/com/ykteknolojistaj/restapi/controller/CustomerLimitController.java
@@ -14,6 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.Pattern;
+
 import java.util.List;
 import java.util.UUID;
 
@@ -34,45 +37,40 @@ public class CustomerLimitController {
 
     /**
      * Get mapping on '/api/getCards' endpoint. Uses one parameter as 'customer_no'
-     * and by using it requesting corresponding cards from the gRPC server.
+     * and by using it requesting corresponding cards from the gRPC server. Exceptions
+     * are handled in exceptionHandler package.
+     *
+     * Example usage: /api/getCards?customer_no=1234567890
      *
      * @param customer_no Customer no parameter from the request
      * @return All cards which belong to the given customer
      */
     @GetMapping(path = "/getCards", produces = "application/json")
-    public List<CardModel> getCardsApi(@RequestParam(value = "customer_no") String customer_no) {
+    public List<CardModel> getCardsApi(@RequestParam(value = "customer_no") @Pattern(regexp = "[1-9]{1}[0-9]{9}") String customer_no, HttpServletRequest request) {
         // initializing empty cardList
         CardList cardList = new CardList();
-
+        request.setAttribute("corrID", "1asdfpıasjfawh9upfhuıfhausd");
+        System.out.println(request);
+        // creating unique id for the request and used it for logging
         String uniqueID = UUID.randomUUID().toString();
+
         LoggingMessage loggingMessage = new LoggingMessage(customer_no, uniqueID, "CustomerLimitController got a request, now calling CardClient", "CustomerLimitController", "start");
         String logMessage = loggingMessage.toString();
         LOG.log(Level.INFO, logMessage);
 
+        //by using customer_no, getting all cards from the gRPC server (customer database microservice)
+        // the return type is protointerface.card class
+        List<Card> cardModels = cardService.receiveCards(customer_no, uniqueID);
 
-        try {
-            // by using customer_no, getting all cards from the gRPC server (customer database microservice)
-            // the return type is protointerface.card class
-            List<Card> cardModels = cardService.receiveCards(customer_no, uniqueID);
-            //System.out.println("Taken cards from gRPC server with " + customer_no + ": " + cardModels);
-            // copying all protointerface.card models to CardModel to use in response
-            cardList.copyProtoCardArray(cardModels);
+        // copying all protointerface.card models to CardModel to use in response
+        cardList.copyProtoCardArray(cardModels);
 
-            LoggingMessage loggingMessage2 = new LoggingMessage(customer_no, uniqueID, "Got a response from CardClient and the returned card(s) are: " + cardList.toString(), "CustomerLimitController", "end");
-            logMessage = loggingMessage2.toString();
-            LOG.log(Level.INFO, logMessage);
-
-        } catch (io.grpc.StatusRuntimeException grpcRuntimeException) {
-            // in case of an error while connecting to the gRPC service
-            //System.out.println("Error while fetching the data from the database microservice.");
-            //System.out.println(grpcRuntimeException);
-            LoggingMessage loggingMessage3 = new LoggingMessage(customer_no, uniqueID, "Error while fetching the data from the database microservice. " + grpcRuntimeException.toString(), "CustomerLimitController", "end");
-            logMessage = loggingMessage3.toString();
-            LOG.log(Level.ERROR, logMessage);
-        }
+        // Loggging the response
+        LoggingMessage loggingMessage2 = new LoggingMessage(customer_no, uniqueID, "Got a response from CardClient and the returned card(s) are: " + cardList.toString(), "CustomerLimitController", "end");
+        logMessage = loggingMessage2.toString();
+        LOG.log(Level.INFO, logMessage);
 
         return cardList.getCards();
     }
-
 
 }
