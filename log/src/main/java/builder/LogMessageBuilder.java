b36a331c70acbf73be80204c56771b63c23ee3d1
emircangun <gundogdu.emircan@outlook.com>
2022-08-14 17:42:38 +0300

    Deleted the unnecessary comment

--- log/src/main/java/builder/LogMessageBuilder.java
+++ log/src/main/java/builder/LogMessageBuilder.java
@@ -8,18 +8,6 @@ public class LogMessageBuilder {
 
     public LogMessageBuilder() {}
 
-//    public LogMessageBuilder(Logger logger,
-//                             String customer_no,
-//                             String corrID,
-//                             String className,
-//                             String humanReadable,
-//                             String part,
-//                             Level level)
-//    {
-//        LogTemplate loggingMessage = new LogTemplate(customer_no, corrID, humanReadable, className, part);
-//        logger.log(level, loggingMessage);
-//    }
-
     public static void Log(Logger logger,
                            String customer_no,
                            String corrID,
