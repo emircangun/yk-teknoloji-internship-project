ff9dd291d1dbc6a661d6976045b57f9470b06588
Emre Uncuoğlu <e.uncuoglu@gmail.com>
2022-08-16 11:22:32 +0300

    DB Unit Test

--- db/src/test/java/CardDaoTest/CardDaoTest.java
+++ db/src/test/java/CardDaoTest/CardDaoTest.java
@@ -4,49 +4,67 @@ import CardEntity.Card;
 import dao.CardDao;
 import jakarta.transaction.Transactional;
 import org.junit.Assert;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.testng.annotations.Test;
 
-
+import java.math.BigDecimal;
 import java.util.List;
 
 @SpringBootTest
 public class CardDaoTest {
 
+    CardDao cardDao = new CardDao();
+    String customerNo;
+    List<Card> cardList;
     @Test
     @Transactional
-    void FindByCustomerNoTest()
+    public void FindByCustomerNoTest_Success()
     {
-        CardDao cardDao = new CardDao();
+        Card card = new Card();
+        card.setCustomerNo("2486751673");
+        card.setCardNo("4916924457536400");
+        card.setAccountNo("753159456");
+        card.setLimit(BigDecimal.valueOf(0.00));
 
-//        Card card = new Card();
-//        card.setCustomerNo("2486751673");
-//        card.setCardNo("4916924457536400");
-//        card.setAccountNo("753159456");
-//        card.setLimit(BigDecimal.valueOf(0.00));
-
-        //Successfull result
-        String customerNo = "2486751673";
-        List<Card> cardList = cardDao.findByCustomerNo(customerNo, "0");
+        customerNo = "2486751673";
+        cardList = cardDao.findByCustomerNo(customerNo, "0");
         Assert.assertNotNull(cardList);
         Assert.assertEquals(2, cardList.size());
 
-        //String input
+        customerNo = "2486751673";
+        cardList = cardDao.findByCustomerNo(customerNo, "0");
+        Assert.assertNotNull(cardList);
+        Assert.assertEquals("2486751673", cardList.get(0).getCustomerNo());
+        cardList.clear();
+    }
+    @Test
+    @Transactional
+    public void FindByCustomerNo_invalidInput()
+    {
         customerNo = "xxx";
         cardList = cardDao.findByCustomerNo(customerNo, "0");
         Assert.assertNotNull(cardList);
         Assert.assertEquals(0, cardList.size());
-
-        //Null result
+        cardList.clear();
+    }
+    @Test
+    @Transactional
+    public void FindByCustomerNo_NullResult()
+    {
         customerNo = "12345";
         cardList = cardDao.findByCustomerNo(customerNo, "0");
         Assert.assertNotNull(cardList);
         Assert.assertEquals(0, cardList.size());
-
-        //Null input
+        cardList.clear();
+    }
+    @Test
+    @Transactional
+    public void FindByCustomerNo_NullInput()
+    {
         customerNo = "";
         cardList = cardDao.findByCustomerNo(customerNo, "0");
         Assert.assertNotNull(cardList);
         Assert.assertEquals(0, cardList.size());
+        cardList.clear();
     }
 }
